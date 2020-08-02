f0d761e2bc2060247c05d940926c5ac416646890
Geordie Herron-Johnson <ge.he.jo@gmail.com>
2020-08-02 12:33:59 -0400

    initial xcode project creation

---
+++ freshVideos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  freshVideos
+//
+//  Created by George Herron-Johnson on 8/2/20.
+//  Copyright © 2020 George Herron-Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
